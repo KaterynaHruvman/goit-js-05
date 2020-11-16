cb166579094047cc134f5c51aa90259a6d672850
KaterynaHruvman <66679134+KaterynaHruvman@users.noreply.github.com>
2020-11-16 15:04:07 +0200

    5

--- JS/task-5.js
+++ JS/task-5.js
@@ -1,44 +1,65 @@
-const inventory = {
-  items: ['Knife', 'Gas mask'],
-  add(itemName) {
-    this.items.push(itemName);
-    return `Adding ${itemName} to inventory`;
-  },
-  remove(itemName) {
-    this.items = this.items.filter(item => item !== itemName);
-    return `Removing ${itemName} from inventory`;
-  },
-};
+class Car {
+  // Write code under this line
+  static getSpecs(car) {
+    console.log(
+      `maxSpeed: ${car.maxSpeed}; Speed: ${car.speed}; isOn: ${car.isOn}; distance: ${car.distance}; price: ${car.price}`
+    );
+  }
 
-const invokeInventoryAction = function(itemName, action) {
-  const act =  action(itemName);
-  const msg =  `Invoking action on ${itemName}`;
-  return {act, msg};
-};
+  constructor({ speed, price, maxSpeed, isOn, distance }) {
+    this.speed = 0;
+    this.price = price;
+    this.maxSpeed = maxSpeed;
+    this.isOn = false;
+    this.distance = 0;
+  }
+  get price() {
+    return this._price;
+  }
+  set price(value) {
+    this._price = value;
+  }
+  turnOn(isOn) {
+    this.isOn = true;
+  }
 
-const invokeAdd = invokeInventoryAction(
-  'Medkit',
-  inventory.add.bind(inventory)  // Write code in this line
-);
-const arrayAdd = [...inventory.items];
+  turnOff() {
+    this.isOn = false;
+    this.speed = 0;
+  }
+  decelerate(value) {
+    if (this.speed - value > 0) {
+     return this.speed -= value;
+    }
+    return this.speed 
+  }
+  accelerate(value) {
+    if (this.speed <= this.maxSpeed) {
+      this.speed += value;
+    }
+  }
+  drive(hours) {
+    if (this.isOn) {
+      this.distance += this.speed * hours;
+    }
+  }
+}
+  
+const mustang = new Car({ maxSpeed: 200, price: 2000 });
+mustang.turnOn();
+mustang.accelerate(50);
+mustang.drive(2);
 
-console.log(invokeAdd);
-//{ act: 'Adding Medkit to inventory', msg: 'Invoking action on Medkit' }
+ console.log(Car.getSpecs(mustang));
+// 'maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000'
 
-console.log(arrayAdd);
-// ['Knife', 'Gas mask', 'Medkit']
+mustang.decelerate(20);
+mustang.drive(1);
+mustang.turnOff();
 
-const invokeRemove = invokeInventoryAction(
-  'Gas mask',
-  inventory.remove.bind(inventory)  // Write code in this line
-);
+console.log(Car.getSpecs(mustang));
+// 'maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000'
 
-const arrayRemove = [...inventory.items];
-
-
-console.log(invokeRemove);
-//{ act: 'Removing Gas mask from inventory', msg: 'Invoking action on Gas mask' }
-
-console.log(arrayRemove);
-// ['Knife', 'Medkit']
-
+// console.log(mustang.price); // 2000
+mustang.price = 4000;
+// console.log(mustang.price); // 4000
